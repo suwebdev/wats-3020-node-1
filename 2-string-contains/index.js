6f7fe7e77b2d638f9ce174ae6d2c2949f9bad3ca
rebeccapeltz <rebeccapeltz@gmail.com>
2019-07-09 15:09:53 -0700

    add 2 string contains

--- 2-string-contains/index.js
+++ 2-string-contains/index.js
@@ -10,20 +10,20 @@ let input = getargs.getStringArg()
 
 // make sure that the user entered a string
 if (!input) {
-  //TODO add a usage statement and let the user know that if a string contains a space it must be quoted
-} 
+  console.log("usage: node 2-string-contains/index.js <string>")
+  console.log("if string contains a space, put it in quotes")
+}
 
 // check to see if string contains hello
 // if it does report back the position in the string where it begins
-
 else {
   // TODO write a logical expression to test if the input contains a "hello"
-  if (){
+  if (input.indexOf("hello") > -1) {
     // log using a template literal at what position the hello was found
-    console.log()
+    console.log(`input contains hello at location ${input.indexOf("hello")}`)
   } else {
     // log that the hello was not found
-    console.log()
+    console.log(`input does not contain the string "hello"`)
   }
 }
 process.exit()
