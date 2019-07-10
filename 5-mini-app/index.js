bd48fc59cffe7bdde83a10a4db30f885e8acc0ce
Rebecca Peltz <rebeccapeltz@gmail.com>
2019-07-10 16:22:01 -0700

    add dyi

---
+++ 5-mini-app/index.js
@@ -0,0 +1,8 @@
+// write an app that meets requirements:
+// * uses if/else
+// * uses at least one operator +, -, *, /, %
+// * reads an arg from the command line and provides usage if not present or wrong type
+// suggestions:
+// * convert Celsius temperature to Fahrenheit F =(C * 9/5) + 32.00
+// * convert Fahrenheit to Celsius C = (F - 32) *  5/9
+// * convert pennies to dollars, for example 136 pennies is $1.36
